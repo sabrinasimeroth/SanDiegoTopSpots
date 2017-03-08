fe03d9e91214807c1628de009044dd7815eb4629
Sabrina  Simeroth <SabrinaSimeroth@Macintosh-3.local>
2017-03-08 12:16:02 -0800

    update to button element and styling

--- topspots.js
+++ topspots.js
@@ -2,10 +2,11 @@ $(document).ready(function(){
   $.getJSON('topspots.json', function(data){
 
     //Create a table from json and append each row to the html table
-    $.each(data, function(i, b){
-      $('#tsTable').append('<tr><td>' + b.name + '</td>' +
-      '<td>' + b.description + '</td>' +
-      '<td>' + '<a href="https://www.google.com/maps/place/' + b.location + '">' + 'Location Map' + '</a></td></tr>');
+    $.each(data, function(i, topspot){
+      $('#tsTable').append('<tr><td>' + topspot.name + '</td>' +
+      '<td>' + topspot.description + '</td>' +
+      '<td>' + '<a id="mapLink" href="https://www.google.com/maps/place/' + topspot.location +
+      '"><button type="button" class="btn btn-primary">' + 'Open Map' + '</button></a></td></tr>');
     });
 
   })
